716d43d34e24e714d94cdc5194ffb94a8e0ebb14
meikuam <meikuam@gmail.com>
2018-01-13 17:56:11 +0700

    HSV fix and some features

--- converter.cpp
+++ converter.cpp
@@ -1,15 +1,15 @@
 #include "converter.h"
 
-uint8_t* Converter::RGB2YCbCr(uint8_t* rgb) {
+uint8_t* Converter::RGB2YCbCr(uint8_t* rgb, bool a, bool b, bool c) {
     int     YCbCr[3];
     double  M[3][3] = {{0.299,     0.587,        0.114},
                        {-0.169,    -0.331,       0.500},
                        {0.500,     -0.419,      -0.081}};
     double  RGB[3];
 
-    RGB[0]   = rgb[0];
-    RGB[1]   = rgb[1];
-    RGB[2]   = rgb[2];
+    RGB[0]   = a ? (double)rgb[0] : 0;
+    RGB[1]   = b ? (double)rgb[1] : 0;
+    RGB[2]   = c ? (double)rgb[2] : 0;
 
     YCbCr[0] = RGB[0] * M[0][0] + RGB[1] * M[0][1] + RGB[2] * M[0][2];
     YCbCr[1] = RGB[0] * M[1][0] + RGB[1] * M[1][1] + RGB[2] * M[1][2] + 128.0;
@@ -24,16 +24,16 @@ uint8_t* Converter::RGB2YCbCr(uint8_t* rgb) {
 
 
 
-uint8_t* Converter::YCbCr2RGB(uint8_t* ycbcr) {
+uint8_t* Converter::YCbCr2RGB(uint8_t* ycbcr, bool a, bool b, bool c) {
     double  M[3][3] = {{1.0,     0.0,        1.402},
                        {1.0,     -0.344,     -0.714},
                        {1.0,     1.722,      0.0}};
     int     RGB[3];
     double  YCbCr[3];
 
-    YCbCr[0] = ycbcr[0];
-    YCbCr[1] = ycbcr[1] - 128.0;
-    YCbCr[2] = ycbcr[2] - 128.0;
+    YCbCr[0] = a ? (double)ycbcr[0] : 0;
+    YCbCr[1] = b ? (double)ycbcr[1] - 128.0 : 0;
+    YCbCr[2] = c ? (double)ycbcr[2] - 128.0 : 0;
 
     RGB[0]   = YCbCr[0] * M[0][0] + YCbCr[1] * M[0][1] + YCbCr[2] * M[0][2];
     RGB[1]   = YCbCr[0] * M[1][0] + YCbCr[1] * M[1][1] + YCbCr[2] * M[1][2];
@@ -46,27 +46,28 @@ uint8_t* Converter::YCbCr2RGB(uint8_t* ycbcr) {
     return rgb;
 }
 
-uint8_t* Converter::RGB2HSV(uint8_t* rgb) {
-    double MAX, MIN;
+uint8_t* Converter::RGB2HSV(uint8_t* rgb, bool a, bool b, bool c) {
+    double MAX = 0, MIN = 0;
     double HSV[3];
     double RGB[3];
-    RGB[0] = rgb[0] / 255.0; // red
-    RGB[1] = rgb[1] / 255.0; // green
-    RGB[2] = rgb[2] / 255.0; // blue
+    RGB[0] = a ? (double)rgb[0] / 255.0 : 0; // red
+    RGB[1] = b ? (double)rgb[1] / 255.0 : 0; // green
+    RGB[2] = c ? (double)rgb[2] / 255.0 : 0; // blue
+
     MAX = max(RGB[0], max(RGB[1], RGB[2]));
-    MAX = min(RGB[0], min(RGB[1], RGB[2]));
+    MIN = min(RGB[0], min(RGB[1], RGB[2]));
 
     if(MAX == MIN)                                  HSV[0] = 0;
-    else if(MAX == RGB[0] && RGB[1] >= RGB[2])      HSV[0] = 60 * (RGB[1] - RGB[2]) / (MAX - MIN);
-    else if(MAX == RGB[0] && RGB[1] <  RGB[2])      HSV[0] = 60 * (RGB[1] - RGB[2]) / (MAX - MIN) + 360.0;
-    else if(MAX == RGB[1])                          HSV[0] = 60 * (RGB[2] - RGB[0]) / (MAX - MIN) + 120.0;
-    else if(MAX == RGB[2])                          HSV[0] = 60 * (RGB[0] - RGB[1]) / (MAX - MIN) + 240.0;
+    else if(MAX == RGB[0] && RGB[1] >= RGB[2])      HSV[0] = 60.0 * ((RGB[1] - RGB[2]) / (MAX - MIN));
+    else if(MAX == RGB[0] && RGB[1] <  RGB[2])      HSV[0] = 60.0 * ((RGB[1] - RGB[2]) / (MAX - MIN)) + 360.0;
+    else if(MAX == RGB[1])                          HSV[0] = 60.0 * ((RGB[2] - RGB[0]) / (MAX - MIN)) + 120.0;
+    else if(MAX == RGB[2])                          HSV[0] = 60.0 * ((RGB[0] - RGB[1]) / (MAX - MIN)) + 240.0;
 
-    HSV[1] = MAX == 0 ? 0 : 1.0 - MIN / MAX;
+    HSV[1] = MAX == 0 ? 0.0 : 1.0 - MIN / MAX;
     HSV[2] = MAX;
 
 
-    HSV[0] = (HSV[0] / 360.0) * 255.0;
+    HSV[0] = (HSV[0] * 255.0)/ 360.0;
     HSV[1] = HSV[1] * 255.0;
     HSV[2] = HSV[2] * 255.0;
 
@@ -77,48 +78,87 @@ uint8_t* Converter::RGB2HSV(uint8_t* rgb) {
     return hsv;
 }
 
-uint8_t* Converter::HSV2RGB(uint8_t* hsv) {
-    double MAX, MIN;
+//uint8_t* Converter::RGB2HSV(uint8_t* rgb, bool a, bool b, bool c) {
+//    double MAX = 0, MIN = 0;
+//    double HSV[3];
+//    double RGB[3];
+//    RGB[0] = a ? (double)rgb[0] / 255.0 : 0; // red
+//    RGB[1] = b ? (double)rgb[1] / 255.0 : 0; // green
+//    RGB[2] = c ? (double)rgb[2] / 255.0 : 0; // blue
+
+//    MAX = max(RGB[0], max(RGB[1], RGB[2]));
+//    MAX = min(RGB[0], min(RGB[1], RGB[2]));
+
+//    if(MAX == MIN)                                  HSV[0] = 0;
+//    else if(MAX == RGB[0] && RGB[1] >= RGB[2])      HSV[0] = 60.0 * (RGB[1] - RGB[2]) / (MAX - MIN);
+//    else if(MAX == RGB[0] && RGB[1] <  RGB[2])      HSV[0] = 60.0 * (RGB[1] - RGB[2]) / (MAX - MIN) + 360.0;
+//    else if(MAX == RGB[1])                          HSV[0] = 60.0 * (RGB[2] - RGB[0]) / (MAX - MIN) + 120.0;
+//    else if(MAX == RGB[2])                          HSV[0] = 60.0 * (RGB[0] - RGB[1]) / (MAX - MIN) + 240.0;
+
+//    HSV[1] = MAX == 0 ? 0.0 : 1.0 - MIN / MAX;
+//    HSV[2] = MAX;
+
+
+//    HSV[0] = (HSV[0] * 1.0 / 360.0) * 255.0;
+//    HSV[1] = HSV[1] * 255.0;
+//    HSV[2] = HSV[2] * 255.0;
+
+//    uint8_t* hsv  = new uint8_t[3];
+//    hsv[0]   = HSV[0] > 255 ? 255 : HSV[0] < 0 ? 0 : (int) HSV[0];
+//    hsv[1]   = HSV[1] > 255 ? 255 : HSV[1] < 0 ? 0 : (int) HSV[1];
+//    hsv[2]   = HSV[2] > 255 ? 255 : HSV[2] < 0 ? 0 : (int) HSV[2];
+//    return hsv;
+//}
+
+uint8_t* Converter::HSV2RGB(uint8_t* hsv, bool a, bool b, bool c) {
     double  RGB[3];
     double  HSV[3];
 
-    HSV[0] = (hsv[0] / 255.0) * 360.0; // hue
-    HSV[1] = hsv[1] / 255.0;           // saturation
-    HSV[2] = hsv[2] / 255.0;           // value
+    HSV[0] = a ? ((double)hsv[0] * 360.0) / 255.0 : 0;   // hue
+    HSV[1] = b ? (double)hsv[1] / 255.0 : 0;           // saturation
+    HSV[2] = c ? (double)hsv[2] / 255.0 : 0;           // value
 
-    double Hi = ((int) (HSV[0] / 60.0)) % 6;
+    int Hi = ((int) (HSV[0]) / 60) % 6;
 
-    double f = (HSV[0] / 60.0)  - ((int) (HSV[0] / 60));
+    double f = HSV[0] / 60.0 - ((int) (HSV[0]) / 60);
 
     double p = HSV[2] * (1.0 - HSV[1]);
     double q = HSV[2] * (1.0 - f * HSV[1]);
     double t = HSV[2] * (1.0 - (1.0 - f) * HSV[1]);
 
-    if(Hi == 0) {
+    switch (Hi) {
+    case 0:
         RGB[0] = HSV[2];
         RGB[1] = t;
         RGB[2] = p;
-    } else if(Hi == 1) {
+        break;
+    case 1:
         RGB[0] = q;
         RGB[1] = HSV[2];
         RGB[2] = p;
-    } else if(Hi == 2) {
+        break;
+    case 2:
         RGB[0] = p;
         RGB[1] = HSV[2];
         RGB[2] = t;
-    } else if(Hi == 3) {
+        break;
+    case 3:
         RGB[0] = p;
         RGB[1] = q;
         RGB[2] = HSV[2];
-    } else if(Hi == 4) {
+        break;
+    case 4:
         RGB[0] = t;
         RGB[1] = p;
         RGB[2] = HSV[2];
-    } else if(Hi == 5) {
+        break;
+    case 5:
         RGB[0] = HSV[2];
         RGB[1] = p;
         RGB[2] = q;
+        break;
     }
+
     RGB[0] *= 255.0;
     RGB[1] *= 255.0;
     RGB[2] *= 255.0;
--- imgdata.cpp
+++ imgdata.cpp
@@ -106,11 +106,25 @@ void ImgData::RGBupdate() {
             uint8_t* rgb;
             for(int x = 0; x < data_->width(); x++) {
                 for(int y = 0; y < data_->height(); y++) {
-                    hsv[0]              = channel_[0] ? *data(x, y, 0) : 0;
-                    hsv[1]              = channel_[1] ? *data(x, y, 1) : 0;
-                    hsv[2]              = channel_[2] ? *data(x, y, 2) : 0;
-                    rgb                 = Converter::HSV2RGB(hsv);
-                    RGBdata_->setPixel(x, y, qRgb(rgb[0], rgb[1], rgb[2]));
+//                    if(channel_[0] && !channel_[1] && !channel_[2]) {
+//                        RGBdata_->setPixel(x, y, qRgb(*data(x, y, 0),
+//                                                      *data(x, y, 0),
+//                                                      *data(x, y, 0)));
+//                    } else if(!channel_[0] && channel_[1] && !channel_[2]) {
+//                        RGBdata_->setPixel(x, y, qRgb(*data(x, y, 1),
+//                                                      *data(x, y, 1),
+//                                                      *data(x, y, 1)));
+//                    } else if(!channel_[0] && !channel_[1] && channel_[2]) {
+//                        RGBdata_->setPixel(x, y, qRgb(*data(x, y, 2),
+//                                                      *data(x, y, 2),
+//                                                      *data(x, y, 2)));
+//                    } else {
+                        hsv[0]              = *data(x, y, 0);
+                        hsv[1]              = *data(x, y, 1);
+                        hsv[2]              = *data(x, y, 2);
+                        rgb = Converter::HSV2RGB(hsv, channel_[0], channel_[1], channel_[2]);
+                        RGBdata_->setPixel(x, y, qRgb(rgb[0], rgb[1], rgb[2]));
+//                    }
 //                    delete rgb;
                 }
             }
@@ -122,11 +136,25 @@ void ImgData::RGBupdate() {
             uint8_t* rgb;
             for(int x = 0; x < data_->width(); x++) {
                 for(int y = 0; y < data_->height(); y++) {
-                    ycbcr[0]            = channel_[0] ? *data(x, y, 0) : 0;
-                    ycbcr[1]            = channel_[1] ? *data(x, y, 1) : 0;
-                    ycbcr[2]            = channel_[2] ? *data(x, y, 2) : 0;
-                    rgb                 = Converter::YCbCr2RGB(ycbcr);
-                    RGBdata_->setPixel(x, y, qRgb(rgb[0], rgb[1], rgb[2]));
+//                    if(channel_[0] && !channel_[1] && !channel_[2]) {
+//                        RGBdata_->setPixel(x, y, qRgb(*data(x, y, 0),
+//                                                      *data(x, y, 0),
+//                                                      *data(x, y, 0)));
+//                    } else if(!channel_[0] && channel_[1] && !channel_[2]) {
+//                        RGBdata_->setPixel(x, y, qRgb(*data(x, y, 1),
+//                                                      *data(x, y, 1),
+//                                                      *data(x, y, 1)));
+//                    } else if(!channel_[0] && !channel_[1] && channel_[2]) {
+//                        RGBdata_->setPixel(x, y, qRgb(*data(x, y, 2),
+//                                                      *data(x, y, 2),
+//                                                      *data(x, y, 2)));
+//                    } else {
+                        ycbcr[0]            = *data(x, y, 0);
+                        ycbcr[1]            = *data(x, y, 1);
+                        ycbcr[2]            = *data(x, y, 2);
+                        rgb                 = Converter::YCbCr2RGB(ycbcr, channel_[0], channel_[1], channel_[2]);
+                        RGBdata_->setPixel(x, y, qRgb(rgb[0], rgb[1], rgb[2]));
+//                    }
 //                    delete rgb;
                 }
             }
